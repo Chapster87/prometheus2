3cc7ba10b65088c1d5bf5fc6a1710d15fe720c97
Andy Chapman <achapm87@gmail.com>
2025-01-29 00:23:44 -0500

    Install Swiper, use within Media Row Component

--- app/components/media/MediaRow.tsx
+++ app/components/media/MediaRow.tsx
@@ -1,4 +1,54 @@
 import MediaCard from "./MediaCard";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+export default function MediaRow({ mediaData }: MediaRowProps) {
+  console.log(mediaData);
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={16}
+      slidesPerView={2}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      breakpoints={swiperBreakpoints}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      {mediaData.map((media: MediaType, index: number) => {
+        return (
+          <SwiperSlide key={index}>
+            <MediaCard mediaData={media} />
+          </SwiperSlide>
+        )
+      })}
+    </Swiper>
+  );
+}
+
+const swiperBreakpoints = {
+  640: {
+    slidesPerView: 4
+  },
+  768: {
+    slidesPerView: 4
+  },
+  1024: {
+    slidesPerView: 5
+  },
+  1280: {
+    slidesPerView: 8
+  },
+  1536: {
+    slidesPerView: 8
+  }
+}
 
 interface MediaType {
   id: number;
@@ -11,17 +61,4 @@ interface MediaType {
 
 interface MediaRowProps {
   mediaData: MediaType[];
-}
-
-export default function MediaRow({ mediaData }: MediaRowProps) {
-  console.log(mediaData);
-  return (
-    <div className="flex gap-4">
-      {mediaData.map((media: MediaType, index: number) => {
-        return (
-          <MediaCard key={index} mediaData={media} />
-        )
-      })}
-    </div>
-  );
 }
